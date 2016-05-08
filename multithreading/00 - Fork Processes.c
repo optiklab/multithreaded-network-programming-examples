9ab5c43a527c12cb77cf6c74c6872c49e4a5628c
optiklab <optiklab@optiklabs-MacBook-Pro.local>
2016-05-08 23:28:45 +0300

    Small changes from my MacOSX. Improved tracing.

--- multithreading/00 - Fork Processes.c
+++ multithreading/00 - Fork Processes.c
@@ -9,14 +9,14 @@
 #define EXIT_FAILURE 1
 
 // Compilation:
-// gcc -std=gnu99 "00 - Processes.c" -o processes
+// gcc -std=gnu99 "00 - Fork Processes.c" -o forkProcesses
 
 // Task:
 // Use fork to create a child process.
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    printf("0 Enter to main!\n");
+    printf("Enter to main!\n");
  	for (int i = 0; i < NUMBER_OF_PROCESSES; i++)
 	{
 		pid_t pid;
@@ -24,25 +24,26 @@ void main(int argc, char *argv[])
 		switch(pid=fork())
 		{
 			case -1:
-				  perror("1 Error of calling fork");
+                  printf("PROC %d:", i);
+                  perror("1 Error of calling fork");
 				  exit(EXIT_FAILURE);
 			case 0:
-				  printf("1 CHILD: Child process %d!\n", getpid());
-				  printf("2 CHILD: Parent PID %d!\n", getppid());
-				  printf("3 CHILD: Wait 10 seconds...\n");
+				  printf("PROC %d: 1 CHILD: Child process %d!\n", i, getpid());
+				  printf("PROC %d: 2 CHILD: Parent PID %d!\n", i, getppid());
+				  printf("PROC %d: 3 CHILD: Wait 10 seconds...\n", i);
                   sleep(10);
-                  printf("4 CHILD: Exit!\n");                  
+                  printf("PROC %d: 4 CHILD: Exit!\n", i);
 				  exit(EXIT_FAILURE);
 			default:
-				  printf("1 PARENT: Parent process %d!\n", getpid());
-				  printf("2 PARENT: Child PID %d\n", pid);
-				  printf("3 PARENT: Wait until child calls exit()...\n");
+				  printf("PROC %d: 1 PARENT: Parent process %d!\n", i, getpid());
+				  printf("PROC %d: 2 PARENT: Child PID %d\n", i, pid);
+				  printf("PROC %d: 3 PARENT: Wait until child calls exit()...\n", i);
 				  waitpid(pid, &status, 0);
-				  printf("4 PARENT: Child exit code: %d\n", WEXITSTATUS(status));
-				  printf("5 PARENT: Exit!\n");
+				  printf("PROC %d: 4 PARENT: Child exit code: %d\n", i, WEXITSTATUS(status));
+				  printf("PROC %d: 5 PARENT: Exit!\n", i);
 		}
 	}
 
-    printf("1 Exit main!\n");
+    printf("Exit main!\n");
     exit(0);
 }
